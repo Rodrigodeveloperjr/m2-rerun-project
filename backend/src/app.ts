048b3c3a7273b5ffcc589b3260bbdc6647dfd8d6
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-12-05 15:16:30 -0300

    middlewares and class AppError added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
+import { handleErrorMiddleware } from './middlewares/handleError.middleware'
+import 'express-async-errors'
 import express from 'express'
 import 'reflect-metadata'
+import 'dotenv/config'
 
 
 const app = express()
 app.use(express.json())
+
+app.use(handleErrorMiddleware)
 
 export { app }
---
+++ backend/src/middlewares/token.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+
+const tokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+
+    let token = req.headers.authorization
+
+    if(!token || !token.includes('Bearer')) {
+
+        return res.status(401).json({ message: 'Invalid token' })
+    }
+
+    token = token.split(' ')[1]
+
+    jwt.verify(token as string, process.env.SECRET_KEY as string, (error: any, decoded: any) => {
+
+        if(error) {
+
+            return res.status(401).json({ message: 'Invalid token' })
+        }
+
+        req.email = decoded.email
+    })
+
+    next()
+}
+
+export { tokenMiddleware }
